cc32f556106aade10a90f4c5d1b705a6cceeeba4
Rahul5742 <rkgamingchannel8@gmail.com>
2024-09-28 20:19:15 +0545

    Stdin_and_Stdout
    Task :
    
    In this challenge, you must read  integers from stdin and then print them to stdout. Each integer must be printed on a new line. To make the problem a little easier, a portion of the code is provided for you in the editor below.
    
    Input Format :
    
    There are  lines of input, and each line contains a single integer.
    
    Sample Input :
    case 1 input: 42
    case 2 input: 100
    case 3 input: 125
    
    Sample Output :
    case 1 output:42
    case 2 output:100
    case 3 output:125

---
+++ Question_no_3__Stdin_and_Stdout.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Question_no_3__Stdin_and_Stdout {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println(" HackerRank Stdin and Stdout Question no 3 :");
+        //case :There are  lines of input, and each line contains a single integer.
+        //input :
+        int a=sc.nextInt();   // case 1 :input int.
+        int b=sc.nextInt();   //case 2 :input int.
+        int c=sc.nextInt();   //case 3 :input int.
+
+        //output :
+
+        System.out.println(a);     // case 1 :output int.
+        System.out.println(b);     // case 2 :output int.
+        System.out.println(c);     // case 3 :output int.
+
+
+
+
+        //Sample Input :
+        //42
+        //100
+        //125
+
+        //--------------------------
+
+        //Sample Output :
+        //42
+        //100
+        //125
+    }
+}
